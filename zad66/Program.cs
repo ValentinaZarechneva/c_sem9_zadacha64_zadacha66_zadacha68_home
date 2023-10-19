482420046e532e154062c992ecf06692a26b5343
Valentina Zarechneva <valvas_07@mail.ru>
2023-10-19 23:44:56 +0300

    add zadacha66

---
+++ zad66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void NumberSum (int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма чисел в промежутке от m до n: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    NumberSum(m, n, sum);
+}
+Console.WriteLine("Введите число m:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+NumberSum(m, n, 0);
+
+
